9958e09d3c4308a688e9467aa4ab0efb0845dd27
Mike Gorton <mike.gorton1@gmail.com>
2019-01-15 17:08:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWHisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase) {
     return "I can't hear you!"
-  } else if (string == string.toUppwerCase) {
+  } else if (string == string.toUpperCase) {
     return "YES INDEED!"
-  }
-  }
+  } else if (string === "I love you, Grandma." {
+    return "I love you, too"
   }
 }
